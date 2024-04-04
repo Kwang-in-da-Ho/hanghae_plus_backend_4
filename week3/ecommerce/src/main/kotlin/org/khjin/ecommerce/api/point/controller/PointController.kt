884793d7f6846364693066bbff80ca5ce13eda96
KwangInDaHo <khjin1197@gmail.com>
2024-04-05 01:24:36 +0900

    week3 - add PointController

---
+++ week3/ecommerce/src/main/kotlin/org/khjin/ecommerce/api/point/controller/PointController.kt
@@ -0,0 +1,20 @@
+package org.khjin.ecommerce.api.point.controller
+
+import org.khjin.ecommerce.api.point.dto.PointChargeRequest
+import org.khjin.ecommerce.api.point.dto.PointChargeResponse
+import org.khjin.ecommerce.api.point.usecase.PointChargeUseCase
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/point")
+class PointController(
+    private val pointChargeUseCase: PointChargeUseCase
+) {
+
+    @PostMapping("/charge")
+    fun charge(@RequestBody pointChargeRequest: PointChargeRequest): PointChargeResponse{
+        return pointChargeUseCase.chargePoint(pointChargeRequest)
+    }
+
+}
